bc426e82e564e437daebc7c5a62ee5feb33e0010
Josefine Magnussen <josefine.magnussen@hotmail.com>
2019-09-26 21:59:39 +0200

    app.vue file

---
+++ Fullstack prosjekter/adminmanager/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <div class="main-container">
+    <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './assets/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+  }
+}
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Be+Vietnam&display=swap');
+
+.main-container{
+  margin-left: 20px;
+}
+  body{
+     margin: 0;
+      font-family: 'Be Vietnam', sans-serif;
+  }
+</style>
