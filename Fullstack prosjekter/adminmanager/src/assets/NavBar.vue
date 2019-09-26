18eb4cd0b070af18923839315f7f8e626c1a4d0e
Josefine Magnussen <josefine.magnussen@hotmail.com>
2019-09-26 21:59:24 +0200

    Navbar created

---
+++ Fullstack prosjekter/adminmanager/src/assets/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <!-- <div class="container"> -->
+      <router-link to="/" class="brand-logo">Employeer Manager</router-link>
+      <!-- </div> -->
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Be+Vietnam&display=swap");
+
+.nav-wrapper {
+  text-align: center;
+  background-color: #049ff9;
+  box-shadow: 0px 0px 9px #0425f9;
+  padding: 10px;
+}
+
+.brand-logo {
+  font-size: 25px;
+  text-decoration: none;
+  /* text-transform: uppercase; */
+  word-spacing: 5px;
+  padding-left: 10px;
+  color: white;
+}
+</style>>
