05150e75a07cc8d1167078475f8efa713f1e8cdc
Josefine Magnussen <josefine.magnussen@hotmail.com>
2019-09-26 21:59:14 +0200

    New Dashbord
    - css add

---
+++ Fullstack prosjekter/adminmanager/src/components/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="dashboard">
+    <h3>Dashboard</h3>
+
+    <div class="fixed-action">
+      <router-link to="/new" class="btn-floating btn-large red">
+        <img src="../assets/img/plus.png" alt />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dashboard",
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped>
+/* Img on the button on the page */
+img {
+  height: 60px;
+  position: absolute;
+  bottom: 8px;
+  right: 16px;
+}
+</style>
