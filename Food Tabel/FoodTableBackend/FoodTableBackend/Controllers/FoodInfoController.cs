a30b74d5ddc4ae567df56bdfca9796936e8053de
Josefine Magnussen <josefine.magnussen@hotmail.com>
2019-10-28 22:00:01 +0100

    Connected the food Info

---
+++ Food Tabel/FoodTableBackend/FoodTableBackend/Controllers/FoodInfoController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using FoodTable.Model;
+using FoodTableBackend.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodTableBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodInfoController : ControllerBase
+    {
+        private readonly FoodDBContext _dbContext;
+        public FoodInfoController(FoodDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET: api/Food
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET: api/Food/5
+        [HttpGet("{FoodId}")]
+        public IEnumerable<FoodInfo> Get(string FoodId)
+        {
+            var conn = new SqlConnection(_dbContext.ConnectionString);
+
+            var sql = "SELECT TheNorwegianFoodTable.FoodID, TheNorwegianFoodTable.Food_Item, FoodCategory " +
+                        "FROM TheNorwegianFoodTable " +
+                        "INNER JOIN TheNorwegianFoodTableCategory " +
+                        "ON TheNorwegianFoodTable.FoodCategory = TheNorwegianFoodTableCategory.FoodId " +
+                        "WHERE TheNorwegianFoodTableCategory.FoodId  = @FoodId";
+
+            return conn.Query<FoodInfo>(sql, new { FoodId = FoodId });
+            ;
+        }
+
+        //// POST: api/Food
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT: api/Food/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Food Tabel/FoodTableBackend/FoodTableBackend/Model/FoodInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodTableBackend.Model
+{
+    public class FoodInfo
+    {
+            public string FoodID { get; set; }
+            public string Food_Item { get; set; }
+            public string Fat { get; set; }
+            public string Kilocalories { get; set; }
+            public string TransFa { get; set; }
+            public string Omega_3 { get; set; }
+            public string Cholesterol { get; set; }
+            public string Carbo { get; set; }
+            public string Sugar { get; set; }
+            public string Dietary_fibre { get; set; }
+            public string Protein { get; set; }
+            public string Salt { get; set; }
+            public string Vitamin_C { get; set; }
+        }
+}
