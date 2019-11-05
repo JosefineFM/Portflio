a1a5c8a4c51dfff9e2f033af6761d60b726c0549
Josefine Magnussen <43983109+JosefineFM@users.noreply.github.com>
2019-11-05 18:04:23 +0100

    Delete firebaseConfig.js

--- adminmanager/src/Firebase/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyCeNnU4LcbtVPzh8y5SV4zE9Z9SLp5p5ow",
-    authDomain: "vue-prod-3615c.firebaseapp.com",
-    databaseURL: "https://vue-prod-3615c.firebaseio.com",
-    projectId: "vue-prod-3615c",
-    storageBucket: "vue-prod-3615c.appspot.com",
-    messagingSenderId: "43732335918",
-    appId: "1:43732335918:web:4552c99892f9ae82e38ebf"
-}
