55029325b94490da02f02155dcdc8c9c2e1d1784
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-01-14 19:09:26 +0100

    Add Swager
    Made it easier and get the hang of the api routes

--- FotoProtfolio/FotoPortfolioBackend/Startup.cs
+++ FotoProtfolio/FotoPortfolioBackend/Startup.cs
@@ -1,18 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using FotoPortfolioBackend;
 using FotoPortfolioBackend.Model;
+using FotoPortfolioBackend.Options;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
 
 namespace FotoPortfolioBackend
 {
@@ -30,9 +26,16 @@ namespace FotoPortfolioBackend
         {
             var connStr = Configuration.GetConnectionString("FotoDb");
             var fotoContext = new FotoContext(connStr);
+            
+    
             services.AddSingleton(fotoContext);
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FotoPortofolioBackend", Version = "v1" });
+            });
 
             //Enable CORS
             services.AddCors(options =>
@@ -46,11 +49,24 @@ namespace FotoPortfolioBackend
                     .WithOrigins("http://localhost:8085");
                 });
             });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "FotoPortofolioBackend V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
