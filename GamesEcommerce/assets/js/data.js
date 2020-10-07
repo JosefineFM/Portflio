f3a2c42c7772c1070029a2fe74b39078055a68e4
Josefine Holth <josefine.holth@hotmail.com>
2020-10-07 21:34:37 +0200

    Save game id in localStorage

--- GamesEcommerce/assets/js/data.js
+++ GamesEcommerce/assets/js/data.js
@@ -1,9 +1,7 @@
-var cart = 1;
-var personObject = { game: "Horizen Zero Dawn™ Complete Edition" };
-var productId = [];
+let cart = 1;
+var gameId = [];
 
-
-function loadData(productId) {
+function loadData() {
     const xhr = new XMLHttpRequest();
     
     xhr.open('GET', 'assets/data/games.json', true);
@@ -11,7 +9,7 @@ function loadData(productId) {
     xhr.onload = function () {
         if (this.status == 200) {
             let gamesData = JSON.parse(this.responseText)
-            console.log(gamesData);
+
 
             let output = '';
 
@@ -20,7 +18,7 @@ function loadData(productId) {
                 ' <img src="' + gamesData[i].image + '" alt="" class="game__img"></img>' + 
                 ' <span class="game__name">' + gamesData[i].title + 
                 '</span>' + ' <span class="game__price">' + gamesData[i].price + '</span>' + 
-                '<button class="button-light add-to-cart" onclick="shoppingCart( '+ gamesData[i].id +')"">Add to Cart</button>' +
+                '<button id="toast" class="button-light add-to-cart" onclick="shoppingCart( '+ gamesData[i].id +')"">Add to Cart</button>' +
                 '</article>'
             }
             document.getElementById('game').innerHTML = output;
@@ -30,13 +28,33 @@ function loadData(productId) {
 }
 
 
-function shoppingCart(productId) { 
-    console.log("Product in shopping cart: " + productId);
+function shoppingCart(gameId) { 
+    
+        // Get the existing data
+        var existing = localStorage.getItem('gameIdArray', gameId);
+    
+        // If no existing data, create an array
+        // Otherwise, convert the localStorage string to an array
+        existing = existing ? existing.split(',') : [];
+    
+        // Add new data to localStorage Array
+        existing.push(gameId);
+    
+        // Save back to localStorage
+        localStorage.setItem('gameIdArray', existing.toString());
+
+
+
+    
+    // console.log('local storage saved data: ' + localStorageId)
+
+
     document.getElementById('cart-count').innerHTML = cart++;
 
     // Save id in sessionStorage - shoppingcart
     // Send id to cart page
-
-    // popup window trigger when product is added to cart
-
+    
 }
+
+
+/* --------------------- See what is in the shoppingCart -------------------- */
