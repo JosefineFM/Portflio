3a86fa2fcf26a6ed88825f37454478c66afcfc20
Josefine Holth <josefine.holth@hotmail.com>
2020-10-08 19:49:47 +0200

    cleand opp js and add getAboutTheGame

--- GamesEcommerce/assets/js/data.js
+++ GamesEcommerce/assets/js/data.js
@@ -1,24 +1,33 @@
 let cart = 1;
-var gameId = [];
+var gamesIds = [];
+var gameId = "";
+
 
 function loadData() {
     const xhr = new XMLHttpRequest();
-    
+
     xhr.open('GET', 'assets/data/games.json', true);
 
     xhr.onload = function () {
         if (this.status == 200) {
             let gamesData = JSON.parse(this.responseText)
-
-
+            
+            
             let output = '';
-
+            
             for (i in gamesData) {
+
+                gameImg = gamesData[i].image;
+                let gameTitle = gamesData[i].title;
+                let gamePrice = gamesData[i].price;
+                let gameId = gamesData[i].id;
+
                 output += ' <article class="game">' + 
-                ' <img src="' + gamesData[i].image + '" alt="" class="game__img"></img>' + 
-                ' <span class="game__name">' + gamesData[i].title + 
-                '</span>' + ' <span class="game__price">' + gamesData[i].price + '</span>' + 
-                '<button id="toast" class="button-light add-to-cart" onclick="shoppingCart( '+ gamesData[i].id +')"">Add to Cart</button>' +
+                ' <img src="' + gameImg + '" alt="" class="game__img"></img>' + 
+                ' <span class="game__name">' + gameTitle + '</span>' + 
+                ' <span class="game__price">' + gamePrice + '</span>' + 
+                '<button class="button-light games__button" onclick="shoppingCart( ' + gameId + ')"">Add to Cart</button>' + 
+                '<button class="button-light games__button" onclick="readMore( ' + gameId + ')"">Red More</button>' + 
                 '</article>'
             }
             document.getElementById('game').innerHTML = output;
@@ -28,33 +37,32 @@ function loadData() {
 }
 
 
-function shoppingCart(gameId) { 
-    
-        // Get the existing data
-        var existing = localStorage.getItem('gameIdArray', gameId);
-    
-        // If no existing data, create an array
-        // Otherwise, convert the localStorage string to an array
-        existing = existing ? existing.split(',') : [];
-    
-        // Add new data to localStorage Array
-        existing.push(gameId);
-    
-        // Save back to localStorage
-        localStorage.setItem('gameIdArray', existing.toString());
+function shoppingCart(gamesIds) { // Get the existing data
+    var existing = localStorage.getItem('gamesIdsArray', gamesIds);
+    // If no existing data, create an array
+    // Otherwise, convert the localStorage string to an array
+    existing = existing ? existing.split(',') : [];
 
+    // Add new data to localStorage Array
+    existing.push(gamesIds);
 
-
-    
-    // console.log('local storage saved data: ' + localStorageId)
-
+    // Save back to localStorage
+    localStorage.setItem('gamesIdsArray', existing.toString());
 
     document.getElementById('cart-count').innerHTML = cart++;
-
-    // Save id in sessionStorage - shoppingcart
-    // Send id to cart page
-    
 }
 
+function readMore(gamesIds) { // Get the existing data
+    // Check browser support
+if (typeof(Storage) !== "undefined") {
+    // Store
+    sessionStorage.setItem("readMoreId", gamesIds);
+  } 
+}
 
 /* --------------------- See what is in the shoppingCart -------------------- */
+
+function getAboutTheGame(){
+    console.log("readMore html: " + sessionStorage.getItem("readMoreId"));
+    document.getElementById("result").innerHTML = sessionStorage.getItem("readMoreId");
+}
