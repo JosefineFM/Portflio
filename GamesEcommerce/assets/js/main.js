aa865edac42baca65cb7a5164dcf2524d063eda9
Josefine Holth <josefine.magnussen@hotmail.com>
2020-10-11 21:26:44 +0200

    moved funtions

--- GamesEcommerce/assets/js/main.js
+++ GamesEcommerce/assets/js/main.js
@@ -2,6 +2,25 @@ let cart = 1;
 var gamesIds = [];
 var gameId = "";
 
+
+/* -------------------------------------------------------------------------- */
+/*                                  MENU SHOW                                 */
+/* -------------------------------------------------------------------------- */
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+            document.getElementById('nav-menu').style.width = "100%";
+        })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu')
+
 function shoppingCart(gamesIds) { // Get the existing data
     var existing = localStorage.getItem('gamesIdsArray', gamesIds);
     // If no existing data, create an array
@@ -30,4 +49,31 @@ if (typeof(Storage) !== "undefined") {
   } 
 
   location.replace("readMore.html")
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
