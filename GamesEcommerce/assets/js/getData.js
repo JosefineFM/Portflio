56e3ca47ad546d801e796bb9af5871b770bce40d
Josefine Holth <josefine.holth@hotmail.com>
2020-10-21 23:07:52 +0200

    fix functions and add addToShoppingCart in this file

--- GamesEcommerce/assets/js/getData.js
+++ GamesEcommerce/assets/js/getData.js
@@ -1,15 +1,16 @@
 function loadData() {
+
+
     const xhr = new XMLHttpRequest();
 
     xhr.open('GET', 'assets/data/games.json', true);
 
     xhr.onload = function () {
         if (this.status == 200) {
-            let gamesData = JSON.parse(this.responseText)
+            gamesData = JSON.parse(this.responseText)
             let output = '';
             let outputTopGames = '';
             let outputNewGames = '';
-
 
             for (i in gamesData) {
 
@@ -21,32 +22,31 @@ function loadData() {
                 let topGames = gamesData[i].topGames;
                 let newGames = gamesData[i].newGame;
                 let gameRelease = gamesData[i].release;
-                
 
-                if(gamefeatured === 1){
-                    output += ' <article class="game">' + 
-                    ' <img src="' + gameImg + '" alt="" class="game__img"></img>' +
-                    ' <span class="game__price">' + gamePrice + '</span>' + 
-                    '<button class="button-light games__button" onclick="shoppingCart( ' + gameId + ')"">Add to Cart</button>' + 
-                    '<button class="button-light games__button" onclick="readMore( ' + gameId + ')"">Red More</button>' + 
-                    '</article>'
-                }
+                obj = {
+                    fn: gameId,
+                    ln: gameTitle
+                };
 
-                if(topGames === 1){
-                    outputTopGames += ' <article class="game">' + 
-                ' <img src="' + gameImg + '" alt="" class="game__img"></img>' +
-                ' <span class="game__price">' + gamePrice + '</span>' + 
-                '<button class="button-light games__button" onclick="shoppingCart( ' + gameId + ')"">Add to Cart</button>' + '<button class="button-light games__button" onclick="readMore( ' + gameId + ')"">Red More</button>' + '</article>'
-                }
-                
-                if(newGames === 1) {
-                    outputNewGames += ' <article class="game">' + 
-                    ' <img src="' + gameImg + '" alt="" class="game__img"></img>' +
-                    ' <span class="game__price"> Release: '  + gameRelease + '</span>' + 
-                    ' <span class="game__price">' + gamePrice + '</span>' + 
+
+                if (gamefeatured === 1) { // console.log("Game Title: " + gameTitle)
+
+
+                    output += ' <article class="game">' + ' <img src="' + gameImg + '" alt="" class="game__img"></img>' + ' <span class="game__price">' + gamePrice + '</span>' + 
+                    '<button class="button-light games__button" onclick="AddToShoppingCart( \'' + gameId + '\',\'' + gameTitle + '\')">Add to Cart</button>' + 
                     '<button class="button-light games__button" onclick="readMore( ' + gameId + ')"">Red More</button>' + '</article>'
                 }
 
+                if (topGames === 1) {
+                    outputTopGames += ' <article class="game">' + ' <img src="' + gameImg + '" alt="" class="game__img"></img>' + ' <span class="game__price">' + gamePrice + '</span>' + 
+                    '<button class="button-light games__button" onclick="AddToShoppingCart( \'' + gameId + '\',\'' + gameTitle + '\')">Add to Cart</button>' + 
+                    
+                    '<button class="button-light games__button" onclick="readMore( ' + gameId + ')"">Red More</button>' + '</article>'
+                }
+
+                if (newGames === 1) {
+                    outputNewGames += ' <article class="game">' + ' <img src="' + gameImg + '" alt="" class="game__img"></img>' + ' <span class="game__price"> Release: ' + gameRelease + '</span>' + ' <span class="game__price">' + gamePrice + '</span>' + '<button class="button-light games__button" onclick="readMore( ' + gameId + ')"">Red More</button>' + '</article>'
+                }
 
 
             }
@@ -55,17 +55,32 @@ function loadData() {
             document.getElementById('newGames').innerHTML = outputNewGames;
 
 
-
-            
         }
     }
     xhr.send();
+    // console.log(localStorage.getItem('shoppingCartItems'))
+    cart = JSON.parse(localStorage.getItem('shoppingCartItems'));
 
-    // var existingInLocalStorage = localStorage.getItem('shoppingCartArray');
-    // var localStorageCount = Array.from(existingInLocalStorage.split(","))
+    document.getElementById('cart-count').innerHTML = cart.length;
+}
 
-    // document.getElementById('cart-count').innerHTML = localStorageCount.length;
+function AddToShoppingCart(gameId, gameTitle) { // Get the existing data
+    let items = {
+        id: gameId,
+        title: gameTitle,
+    };
 
-    /* ---------------------------------- news ---------------------------------- */
+    cart = [];
+
+    // Parse the serialized data back into an aray of objects
+    cart = JSON.parse(localStorage.getItem('shoppingCartItems')) || [];
+
+    // Push the new data (whether it be an object or anything else) onto the array
+    cart.push(items);
+
+    // Re-serialize the array back into a string and store it in localStorage
+    localStorage.setItem('shoppingCartItems', JSON.stringify(cart));
+    
+    document.getElementById('cart-count').innerHTML = cart.length;
 
 }
