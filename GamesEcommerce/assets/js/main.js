4c8079f0dac027e4815fc462c9c0d64f9232854a
Josefine Holth <josefine.magnussen@hotmail.com>
2020-10-27 23:58:10 +0100

    change the slideshow functions

--- GamesEcommerce/assets/js/main.js
+++ GamesEcommerce/assets/js/main.js
@@ -30,7 +30,7 @@ if (typeof(Storage) !== "undefined") {
   location.replace("readMore.html")
 }
 
-var slideIndex = 1;
+var slideIndex = 0;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -45,17 +45,17 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    slides[i].style.display = "none";  
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000);
+  setTimeout(showSlides, 40000); // Change image every 2 seconds
 }
 
 function readAboutAllDlcs(){
